08f7c3e2921221d90dd9f8a896bb9a42f128fd6b
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-11-25 16:43:45 +0530

    ADDED: Refactore Code To Write Function For Dailly Working Hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,4 +1,4 @@
-// UC2: Calcuate Dailly Employee Wage
+// UC3: Refactore the code to Write a Function to get Work Hour
 //Constant
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -6,19 +6,24 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const RATE_PER_HOUR = 20;
 
-// Variables
-let empHrs = 0,
- empCheck = Math.floor(Math.random() * 10) % 3; 
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs = 0;
+//fuction used to check part or full time
+function getWorkingHours(empCheck){
+    switch (empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            break;
+         case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+            break;
+        default :
+            return 0;
+    }
 }
+let empHrs = 0;
+// get Random Values
+let empCheck = Math.floor(Math.random() * 10) % 3; 
+// calculate Emp Hrs
+empHrs = getWorkingHours(empCheck);
 // Calculate Employee Wages
-empWage = empHrs * RATE_PER_HOUR;
+let empWage = empHrs * RATE_PER_HOUR;
 console.log("Employee Wage: " + empWage);
