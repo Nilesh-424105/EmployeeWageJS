bab1be80eac50f6a5e11a431a8e99fb666b4fc09
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-11-25 16:09:13 +0530

    ADDED: To Check Employee Is Present Or Absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,7 @@
-console.log("Welcome to Employee Wage Java Script Program")
+//UC 1: Employee is Present or Absent
+let IS_PRESENT = 1;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == 1)
+    console.log("Employee is PRESENT");
+else
+    console.log("Employee is ABSENT");
