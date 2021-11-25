2c88c3987dbd1dfe1291aeec4fdb1f409ae21a11
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-11-25 17:08:50 +0530

    ADDED: To Calculate Employee Wages For a Month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,11 @@
-// UC3: Refactore the code to Write a Function to get Work Hour
+// UC4: Calculating Wages For a Month
 //Constant
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const RATE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 //fuction used to check part or full time
 function getWorkingHours(empCheck){
@@ -20,10 +21,13 @@ function getWorkingHours(empCheck){
     }
 }
 let empHrs = 0;
-// get Random Values
-let empCheck = Math.floor(Math.random() * 10) % 3; 
-// calculate Emp Hrs
-empHrs = getWorkingHours(empCheck);
-// Calculate Employee Wages
+// Calculate total working hours
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    // get random values
+    let empCheck = Math.floor(Math.random() * 10) % 3; 
+    // calculate emp Hrs
+    empHrs += getWorkingHours(empCheck);
+}
+// Calculate Employee Monthly Wages
 let empWage = empHrs * RATE_PER_HOUR;
-console.log("Employee Wage: " + empWage);
+console.log("Total Working Hours: " + empHrs +"\nEmployee Monthly Wages: " + empWage);
